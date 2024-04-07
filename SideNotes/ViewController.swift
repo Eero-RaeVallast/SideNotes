725eb4e3c79c6b541b144cf994df57dcbaa521c3
Eero Männik <eeromannik@gmail.com>
2024-04-07 19:28:02 +0300

    Initial Commit

---
+++ SideNotes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SideNotes
+//
+//  Created by Eero Mannik on 07.04.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
